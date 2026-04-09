90945ce5d4634ea582c47a230f47b3cea3a108c8
Pranay Reddy Kunta <easynotes008@gmail.com>
2026-04-09 12:00:00 +0530

    Home viewmodel updated with Room database usage

--- app/src/main/java/uk/ac/tees/mad/easynotes/data/local/DatabaseProvider.kt
+++ app/src/main/java/uk/ac/tees/mad/easynotes/data/local/DatabaseProvider.kt
@@ -1,2 +1,22 @@
 package uk.ac.tees.mad.easynotes.data.local
 
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+
+    @Volatile
+    private var INSTANCE: EasyNotesDatabase? = null
+
+    fun getDatabase(context: Context): EasyNotesDatabase {
+        return INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                EasyNotesDatabase::class.java,
+                "easynotes_database"
+            ).build()
+            INSTANCE = instance
+            instance
+        }
+    }
+}
